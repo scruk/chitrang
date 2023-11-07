d850b7de352231763daa804d2055da2e1c89f1ca
phuctho509 <132209931+scruk@users.noreply.github.com>
2023-11-07 10:39:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,5 +14,5 @@ const CONFIG = {
     mess: 'Em biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
     messDesc: 'Em chào chị, là đứa em dô tri của chị đây, khoảng thời gian 1 năm qua rất đặc biệt với em khi được gặp chị và mọi người trong SUN. Em rất tự hào khi được làm mentee của chị, người bạn, người chị của em, người luôn cho em những lời khuyên trong những vấn đề mà em gặp phải. Em cảm ơn chị rất nhiều vì khoảng thời gian vừa qua và mong chị sẽ tiếp tục cho em thật nhiều những lời khuyên. Chúc chị thật nhiều sức khỏe, ngày càng xinh đẹp và thành công hơn nữa nhé!',
     btnAccept: 'Nhấn vào đây nếu chị thấy sến',
-    messLink: 'https://youtu.be/vpRi8S6uXAg?feature=shared&t=177' 
+    messLink: 'https://youtube.com/shorts/nD_5n64c2PQ?feature=shared' 
 }
