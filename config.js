a6e01711dc6477faf1162cdbb2120668c9359804
phuctho509 <132209931+scruk@users.noreply.github.com>
2023-11-07 10:04:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Xin chào các SUNer',
+    introTitle: 'Em chào chị Trang',
     introDesc: `Từ lần đầu tiên gặp nhau
     em/tui đã nghĩ chúng mình là 
     My Đẹt ty ni của nhau rồi`,
@@ -8,11 +8,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'chắc chắn là như dậy rồi <33',
     btnNo: 'Không, tao không thấy thế :3',
-    question:'Cho em/tui biết lý do mọi người nghĩ như vậy với ạ',
+    question:'Cho em biết lý do chị nghĩ như vậy với ạ',
     btnReply: 'Nè em',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em/tui biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
-    messDesc: 'Như đã nói ở trên thì em/tui nghĩ chúng ta gặp được nhau đã là định mệnh. Tương lai thì không nói trước được nhưng em/tui luôn mong mọi người hãy luôn trao cho nhau những tình cảm, những yêu thương cho nhau như ngày đầu nhé!',
-    btnAccept: 'Nhấn vào đây để nhận quà từ em/tui',
-    messLink: 'https://drive.google.com/drive/folders/1aKTyla_rG11DD0adIaZV4ExinLbC6bJ5?usp=sharing' 
+    mess: 'Em biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
+    messDesc: 'Em chào chị, là đứa em dô tri của chị đây, khoảng thời gian 1 năm qua rất đặc biệt với em khi được gặp chị và mọi người trong SUN. Em rất tự hào khi được làm mentee của chị, người bạn, người chị của em, người luôn cho em những lời khuyên trong những vấn đề mà em gặp phải. Em cảm ơn chị rất nhiều vì khoảng thời gian vừa qua và mong chị sẽ tiếp tục cho em thật nhiều những lời khuyên. Chúc chị thật nhiều sức khỏe, ngày càng xinh đẹp và thành công hơn nữa nhé!',
+    btnAccept: 'Nhấn vào đây nếu chị thấy sến',
+    messLink: 'https://youtu.be/vpRi8S6uXAg?feature=shared&t=177' 
 }
